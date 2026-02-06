4c7e56c831c62b282006cc074316b1b1279a583e
CCrriimm <riancliffordxd@gmail.com>
2026-02-07 06:14:10 +0800

    Bulaga

---
+++ JS/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getDatabase, ref, push, onValue, set, get, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcRdkDOLrTCGNfBWzXMid1bYzQ_x-zzrg",
+    authDomain: "tomschicken-52fae.firebaseapp.com",
+    databaseURL: "https://tomschicken-52fae-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "tomschicken-52fae",
+    storageBucket: "tomschicken-52fae.firebasestorage.app",
+    messagingSenderId: "1071509846515",
+    appId: "1:1071509846515:web:431fb88f4b7b213eca7a86",
+    measurementId: "G-CJ4SKQD5NV"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+export { app, db, auth, ref, push, onValue, set, get, update, signInWithEmailAndPassword, signOut, onAuthStateChanged };
